db03eca3117e1dee1f21c51090a0c13302d1d2a1
Chen Asraf <casraf@pm.me>
2025-06-07 02:20:46 +0300

    chore: formatting/lint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{ts,vue}': ['eslint --fix'],
-  '*.{scss,vue,ts,md,json}': ['prettier --write'],
+  'src/*.{scss,vue,ts,md,json}': ['prettier --write'],
   '*.php': [
     'php vendor-bin/cs-fixer/vendor/php-cs-fixer/shim/php-cs-fixer.phar --config=.php-cs-fixer.dist.php fix',
   ],
